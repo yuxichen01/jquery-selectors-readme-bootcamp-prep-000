984701a2698dc10eef141c85205b6b25605dc526
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-04 00:31:35 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,6 +5,7 @@ function paragraphSelector() {
 function lastImageSelector() { 
   return $('img:last')
 }
-function ninjaBabySelector(){
+function ninjaBabySelector() {
   return $('#baby-ninja')
 }
+function divSelector()
